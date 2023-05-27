488d510ff7630f9399c820cecd24711d1cec04c8
Agurchkova <copyart@mail.ru>
2023-05-28 01:30:50 +0300

    ref5

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const { NODE_ENV, JWT_SECRET = 'JWT_SECRET' } = process.env;
-const { PORT = 'https://api.agurchkova.mesto15.nomoredomains.monster' } = process.env;
+const { PORT = 3000 } = process.env;
 const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 module.exports = {
